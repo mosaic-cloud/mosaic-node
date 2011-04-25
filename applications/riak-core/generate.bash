2c401a2ca488bfa154b84c4f37557be54fb78d45
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-04-25 13:27:25 +0300

    Removed non-essential `basho-stats` dependency.

--- applications/riak-core/generate.bash
+++ applications/riak-core/generate.bash
@@ -11,6 +11,5 @@ rm -Rf ./generated
 mkdir ./generated
 
 cp -T ./repositories/riak-core/ebin/riak_core.app ./generated/riak_core.app
-cp -T ./repositories/basho-stats/ebin/basho_stats.app ./generated/basho_stats.app
 
 exit 0
