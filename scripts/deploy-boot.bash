18a6c33d43a37a7949b818f4be3c0c56ef25d5ba
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-01-13 16:15:02 +0200

    Rename `publish` script as `deploy`

--- scripts/deploy-boot.bash
+++ scripts/deploy-boot.bash
@@ -5,7 +5,7 @@ if ! test "${#}" -eq 0 ; then
 	exit 1
 fi
 
-if test "${_mosaic_publish_cook:-true}" == true ; then
+if test "${_mosaic_deploy_cook:-true}" == true ; then
 	test -e "${_outputs}/package.tar.gz"
 	ssh -T "${_package_cook}" <"${_outputs}/package-boot.tar.gz"
 fi
