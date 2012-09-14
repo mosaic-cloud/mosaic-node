705e737161abe2304f578d80450305c93f0054bc
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-09-14 15:31:12 +0300

    Add process `examine` and `map` frontend functions to the tools module

--- applications/mosaic-node/sources/mosaic_cluster_processes.erl
+++ applications/mosaic-node/sources/mosaic_cluster_processes.erl
@@ -2,8 +2,9 @@
 -module (mosaic_cluster_processes).
 
 
--export ([resolve/1, define/4, create/1, stop/1, stop/2, list/0]).
+-export ([resolve/1, resolve/0, define/4, examine/1, examine/0, create/1, stop/1, stop/2]).
 -export ([define_and_create/3, define_and_create/4]).
+-export ([list/0, map/1]).
 -export ([service_nodes/0, service_activate/0, service_deactivate/0, service_ping/0, service_ping/1]).
 
 
@@ -34,10 +35,68 @@ resolve (Keys)
 			end,
 			Keys, mosaic_cluster_processes, mosaic_cluster_processes_vnode, 1).
 
+resolve () ->
+	case list () of
+		Outcome = {ok, [], ListReasons} when is_list (ListReasons) ->
+			Outcome;
+		{ok, Keys, ListReasons} when is_list (Keys), is_list (ListReasons) ->
+			case resolve (Keys) of
+				{ok, Processes, ResolveReasons} ->
+					{ok, Processes, ListReasons ++ ResolveReasons};
+				Error = {error, _Reason} ->
+					Error
+			end;
+		Error = {error, _Reason} ->
+			Error
+	end.
+
+
+examine (Key)
+		when is_binary (Key), (bit_size (Key) =:= 160) ->
+	case examine ([Key]) of
+		{ok, [{Key, Details}], []} ->
+			{ok, Details};
+		{ok, [], [{_Target, Key, Reason}]} ->
+			{error, Reason};
+		{ok, [], []} ->
+			{error, unreachable_vnode}
+	end;
+	
+examine (Keys)
+		when is_list (Keys), (Keys =/= []) ->
+	mosaic_cluster_tools:service_request_reply_sync_command (
+			fun (Key) -> {mosaic_cluster_processes, examine, Key} end,
+			fun (Key, _Request, Reply) ->
+				case Reply of
+					{ok, Details} ->
+						{outcome, {Key, Details}};
+					Error = {error, _Reason} ->
+						Error;
+					_ ->
+						{error, {invalid_reply, Reply}}
+				end
+			end,
+			Keys, mosaic_cluster_processes, mosaic_cluster_processes_vnode, 1).
+
+examine () ->
+	case list () of
+		Outcome = {ok, [], ListReasons} when is_list (ListReasons) ->
+			Outcome;
+		{ok, Keys, ListReasons} when is_list (Keys), is_list (ListReasons) ->
+			case examine (Keys) of
+				{ok, Details, ExamineReasons} ->
+					{ok, Details, ListReasons ++ ExamineReasons};
+				Error = {error, _Reason} ->
+					Error
+			end;
+		Error = {error, _Reason} ->
+			Error
+	end.
+
 
 define (Type, ConfigurationEncoding, ConfigurationContent, Key)
 		when is_atom (Type), is_atom (ConfigurationEncoding), is_binary (Key), (bit_size (Key) =:= 160) ->
-	case define (Type, ConfigurationEncoding, ConfigurationContent, Key) of
+	case define (Type, ConfigurationEncoding, ConfigurationContent, [Key]) of
 		{ok, [Key], []} ->
 			ok;
 		{ok, [], [{_Target, Key, Reason}]} ->
@@ -166,6 +225,9 @@ define_and_create (Type, ConfigurationEncoding, ConfigurationContent, Count)
 
 list () ->
 	mosaic_cluster_tools:service_list_sync_command (mosaic_cluster_processes, mosaic_cluster_processes_vnode).
+
+map (Mapper) ->
+	mosaic_cluster_tools:service_map_sync_command (mosaic_cluster_processes, mosaic_cluster_processes_vnode, Mapper).
 
 service_nodes () ->
 	mosaic_cluster_tools:service_nodes (mosaic_cluster_processes).
