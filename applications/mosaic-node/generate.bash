78104e5cdd587db400c19731dc3016e566f40ec4
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-10-11 16:03:49 +0300

    Minor update

--- applications/mosaic-node/generate.bash
+++ applications/mosaic-node/generate.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e -E -u -o pipefail || exit 1
+set -e -E -u -o pipefail -o noclobber -o noglob -o braceexpand || exit 1
+trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
+
 test "${#}" -eq 0
 
 cd -- "$( dirname -- "$( readlink -e -- "${0}" )" )"
