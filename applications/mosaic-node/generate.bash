ed4888ff634643a0e8dd1d69604cca803a768085
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-11-09 03:38:49 +0200

    Update generate scripts to use temporary folder

--- applications/mosaic-node/generate.bash
+++ applications/mosaic-node/generate.bash
@@ -6,9 +6,7 @@ trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
 test "${#}" -eq 0
 
 cd -- "$( dirname -- "$( readlink -e -- "${0}" )" )"
-
-rm -Rf ./.generated
-mkdir ./.generated
+test -d ./.generated
 
 (
 	cat <<'EOS'
